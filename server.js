1807f0618689b98ec518a76dbb5c41735d28d1db
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-03-17 12:36:24 +0000

    Chalk adding title

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const mysql = require("mysql")
 const consoleTable = require('console.table');
 const inquirer = require("inquirer")
 const express = require("express")
+const chalk = require("chalk")
+chalk.enabled = true
+console.log(chalk.blueBright('I am working'));
 
-// Creating connection
+// Creating connection to sql database
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -16,6 +19,35 @@ const connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err
-    console.log("Connected ID" + connection.threadId)
+    console.log("Connected ID " + connection.threadId)
     startInq();
 });
+
+
+console.log(chalk.black.bgCyanBright(`_______________________________________________
+╔═╗┌┬┐┌─┐┬  ┌─┐┬ ┬┌─┐┌─┐  ╔╦╗┌─┐┌┐┌┌─┐┌─┐┌─┐┬─┐
+║╣ │││├─┘│  │ │└┬┘├┤ ├┤   ║║║├─┤│││├─┤│ ┬├┤ ├┬┘
+╚═╝┴ ┴┴  ┴─┘└─┘ ┴ └─┘└─┘  ╩ ╩┴ ┴┘└┘┴ ┴└─┘└─┘┴└─
+____Select an option below to get started _____
+_______________________________________________
+`));
+
+startInq() 
+  inquirer.prompt([
+  {
+  type: "list",
+  message: "What would you like to do?",
+  name: "choice",
+  choices: [
+            "View All Employees?", 
+            "View All Employee's By Roles?",
+            "View all Emplyees By Deparments", 
+            "Update Employee",
+            "Add Employee?",
+            "Add Role?",
+            "Add Department?"
+          ]
+}
+  ])
+
+
