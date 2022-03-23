cafabb43977e39ff3fedf112b4b3f14419ef1785
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-03-23 14:54:31 +0000

    Add Employee

--- server.js
+++ server.js
@@ -89,13 +89,13 @@ const startInq = () => {
   });
 };
 
-// VIEW
-
+// VIEW ----------------------------------------------------------------------------------------------------------------------------------
 // View All Employees
 function viewAllEmployees() {
   connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;", 
   function(err, res) {
     if (err) throw err
+    console.log(`All Employees:`);
     console.table(res)
     startInq()
 })
@@ -106,6 +106,7 @@ function viewAllRoles() {
 connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", 
 function(err, res) {
 if (err) throw err
+console.log(`All Roles:`);
 console.table(res)
 startInq()
 })
@@ -114,10 +115,83 @@ startInq()
 
 // View All Departments
 function viewAllDepartments() {
-connection.query("SELECT department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;", 
+connection.query("SELECT department.id AS id, department.name AS department FROM department;", 
 function(err, res) {
+if (err) throw err
+    console.log(`All Departments:`);
+    console.table(res)
+    startInq();
+  });
+};
+
+// MENU/SELECT -------------------------------------------------------------------------------------------------------------------------------
+// Creating a roles array 
+var rolesArray = [];
+function selectRole() {
+connection.query("SELECT * FROM role", function(err, res) {
   if (err) throw err
-  console.table(res)
-  startInq()
+  for (var i = 0; i < res.length; i++) {
+    rolesArray.push(res[i].title);
+  }
+})
+return rolesArray;
+}
+
+// If the employee does not have a manager (null), it means they have manager status
+var managersArray = [];
+function selectManager() {
+connection.query("SELECT first_name, last_name FROM employee WHERE manager_id IS NULL", function(err, res) {
+  if (err) throw err
+  for (var i = 0; i < res.length; i++) {
+    managersArray.push(res[i].first_name);
+  }
+
+})
+return managersArray;
+}
+
+//ADD-------------------------------------------------------------------------------------------------------------------------------
+//Add an Employee
+function addEmployee() { 
+  inquirer.prompt([
+      {
+        name: "firstName",
+        type: "input",
+        message: "Enter a first name"
+      },
+      {
+        name: "lastName",
+        type: "input",
+        message: "Enter a last name"
+      },
+      {
+        name: "role",
+        type: "list",
+        message: "Enter the role title",
+        choices: selectRole()
+      },
+      {
+          name: "choice",
+          type: "rawlist",
+          message: "Select a manager",
+          choices: selectManager()
+      }
+
+  ]).then(function (val) {
+    let roleId = selectRole().indexOf(val.role) + 1
+    let managerId = selectManager().indexOf(val.choice) + 1
+    connection.query("INSERT INTO employee SET ?", 
+    {
+        first_name: val.firstName,
+        last_name: val.lastName,
+        manager_id: managerId,
+        role_id: roleId
+        
+    }, function(err){
+        if (err) throw err
+        console.table(val)
+        startInq()
+    })
 })
 }
+
